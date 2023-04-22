3db302c4a60af4b044626aed9d6f5bed2c353786
Chernenko Ruslan <ractyfree@gmail.com>
2023-04-22 22:42:48 +0300

    Make wget quiet

--- download_model.sh
+++ download_model.sh
@@ -1,3 +1,3 @@
-wget $MODEL_URL -O model.pth -q --show-progress
-wget $TOKENIZER_URL -O tokenizer.json -q --show-progress
+wget $MODEL_URL -O model.pth -q
+wget $TOKENIZER_URL -O tokenizer.json -q
 
